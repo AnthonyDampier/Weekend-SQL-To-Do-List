c293be68921ab2ec1c9e3a289ae1a8a602d3a41f
AnthonyDampier <Dampieranthony2@gmail.com>
2022-11-11 16:01:39 -0600

    Configured HTML and DB

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,7 +3,43 @@ $(document).on(onReady());
 function onReady(){
     console.log('jQuery sourced.');
     refreshToDoList();
+    $('#enter-btn').on('click', addToDo);
+    
 };
+
+function addToDo(){
+    console.log('button enter');
+    // grab values
+    title = $('#text-input').val();
+    console.log(title);
+    date = $('#date-input').val();
+    console.log(date);
+    time = $('#time-input').val();
+    console.log(time);
+    favorite = false;
+    // PUT object route to DB
+    newToDo = {
+        title: title,
+        date: date,
+        time: time,
+        favorite: favorite
+    }
+    console.log(newToDo)
+    $.ajax({
+		method: 'POST',
+		url: '/toDo',
+		data: newToDo
+	}).then(function (response) {
+		console.log('...back from POSTING to DataBase');
+		//console.log(‘This is what was sent to DB: ‘, response);
+		$('#text-input').val('');
+		$('#date-input').val('');
+		$('#time-iput').val('');
+		refreshToDoList();
+	}).catch(function (error) {
+		alert('SaveKoala function has failed to POST to DataBase!');
+	});
+}
 
 function refreshToDoList() {
     console.log('refresh to do list');
@@ -24,6 +60,7 @@ function renderToDoList(toDoList) {
     for(let i = 0; i < toDoList.length; i += 1) {
         let toDo = toDoList[i];
         console.log(toDo);
+        console.log(toDo.time);
       // For each book, append a new row to our table
         $('.to-do-list').append(`
         <div class="task">
@@ -35,7 +72,12 @@ function renderToDoList(toDoList) {
                 <input 
                     type="text"
                     class="date"
-                    value="${toDo.dueDate}"
+                    value="${(Date(toDo.dueDate)).slice(0, 15).replace('T', ' ')}"
+                    readonly>
+                <input
+                    type="time"
+                    class="time"
+                    value="${toDo.time}"
                     readonly>
                 <button class="edit" data-index="${i}" data-id="${toDo.id}">Edit</button>
                 <button class="delete" data-id="${toDo.id}">Delete</button>
@@ -43,32 +85,3 @@ function renderToDoList(toDoList) {
     `);
     }
 }
-
-//   function deleteBook(){
-//     console.log('in deleteBook()');
-//     const bookID = $(this).data('id');
-//     $.ajax({
-//       method: 'DELETE',
-//       url: `/books/${bookID}`
-//     })
-//     .then( function() {
-//       refreshBooks();
-//     }).catch( function(error){
-//       alert(`deleteBook errored: ${error}`);
-//     });
-//   }
-  
-//   function isReadBook(){
-//     //toggle is read in database to true
-//     console.log('in isReadBook()');
-//     const bookID = $(this).data('id');
-  
-//     $.ajax({
-//       method: 'PUT',
-//       url: `/books/isRead/${bookID}`,
-//     }).then( function (){
-//       refreshBooks();
-//     }).catch( function(error){
-//       console.log('Error in isReadBook ', error);
-//     })
-//   }
